bb9e8e90f99daff019c5d3bd642cd5ae1a5036fb
capugafl <capugafl@ittepic.edu.mx>
2018-06-02 09:20:58 -0600

    home Scanner

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  //public contacts: any = [];
 
+  value = {
+    name: '',
+    phone: ''
+    //,email: ''
+  };
+
+  QRCode;
+
+  constructor(public navCtrl: NavController,
+    private barcodeScanner: BarcodeScanner,
+    private contacts: Contacts,
+    public alertCtrl: AlertController) { }
+
+  scan() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      if (barcodeData.cancelled) return;
+      //this.contacts.push(new Object(JSON.parse(barcodeData.text)));
+      let readedContact = new Object(JSON.parse(barcodeData.text));
+
+      let contact: Contact = this.contacts.create();
+      contact.name = new ContactName(null, null, JSON.parse(barcodeData.text).name);
+      contact.phoneNumbers = [new ContactField('mobile', JSON.parse(barcodeData.text).phone)];
+      //contact.emails = [new ContactField('email', JSON.parse(barcodeData.text).email)];
+      contact.save().then(
+        () => alert('Contact saved!'),
+        (error: any) => alert('Error saving contact.')
+      );
+
+    }).catch(err => {
+      alert(err.toString());
+    });
   }
-
-}
